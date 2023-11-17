7d654e5398b8bdb32e1a188423b92dd397296f9a
Nacho Antunes <ignacioantunes@outlook.com>
2023-11-17 01:25:24 -0300

    :)

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -1,3 +1,10 @@
 export const ItemDetail = ({item}) =>{
-    return(1)
+    return(
+        <>
+            <h1>{item.title}</h1>
+            <div>{item.description}</div>
+            <div>{item.price}</div>        
+        </>
+    )
+
 }
